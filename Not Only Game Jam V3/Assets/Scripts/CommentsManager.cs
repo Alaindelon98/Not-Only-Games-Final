b5b5c83876fdcd29c8b070875b5843de4a307620
Joan Cuch <stauas@gmail.com>
2018-05-13 03:25:23 +0200

    V3

--- Not Only Game Jam V3/Assets/Scripts/CommentsManager.cs
+++ Not Only Game Jam V3/Assets/Scripts/CommentsManager.cs
@@ -17,6 +17,8 @@ public class CommentsManager : MonoBehaviour {
 
     public float l_moveSpace;
 
+    int currentPhoto;
+
     private int l_newLinesOcuped;
 
 
@@ -31,8 +33,36 @@ public class CommentsManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Pressed");
+            currentPhoto++;
+            if (currentPhoto >= 8)
+            {
+                currentPhoto = 0;
+            }
+
+            char charScene = '0';
+
+            switch (currentPhoto)
+            {
+                case 0: currentPhoto = '0'; break;
+                case 1: currentPhoto = '1'; break;
+                case 2: currentPhoto = '2'; break;
+                case 3: currentPhoto = '3'; break;
+                case 4: currentPhoto = '4'; break;
+                case 5: currentPhoto = '5'; break;
+                case 6: currentPhoto = '6'; break;
+                case 7: currentPhoto = '7'; break;
+
+            }
+
+            SpawnComments(charScene);
+            Debug.Log("int " + currentPhoto);
+            Debug.Log(charScene);
+        }
+    }
+
 
     public void SpawnComments(char l_sceneChar)
     {
@@ -47,6 +77,10 @@ public class CommentsManager : MonoBehaviour {
 
     IEnumerator SpawnNextComment()
     {
+        foreach (Text text in l_spawnedList)
+        {
+            Destroy(text.gameObject);
+        }
         l_spawnedList.Clear();
 
         //for (int idx = l_commentsList.Count-1; idx >= 0; idx--)
