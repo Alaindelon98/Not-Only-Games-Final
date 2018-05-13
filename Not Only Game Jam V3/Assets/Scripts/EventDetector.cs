b46acaa5700c68f9e69c75061ef6994b2b168a3a
Joan Cuch <stauas@gmail.com>
2018-05-13 06:01:58 +0200

    a

--- Not Only Game Jam V3/Assets/Scripts/EventDetector.cs
+++ Not Only Game Jam V3/Assets/Scripts/EventDetector.cs
@@ -26,9 +26,20 @@ public class EventDetector : MonoBehaviour {
         Vector3 l_halfExtents = m_screenShot.F_GetRenderCameraOrthograficSize();
         l_halfExtents.z = l_halfExtents.x;
 
-        Collider[] l_colliders = Physics.OverlapBox(_position, l_halfExtents);
+        //Collider[] l_colliders = Physics.OverlapBox(_position, l_halfExtents);
+        Collider2D[] l_colliders = Physics2D.OverlapBoxAll(new Vector2(_position.x, _position.y), new Vector2(6, 3), 0);
+        //Collider2D[] l_colliders = Physics2D.OverlapCircleAll(new Vector2(_position.x, _position.y), 1.5f);
+        //GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        //GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //sphere.GetComponent<SphereCollider>().radius = l_halfExtents.x;
+        //sphere.transform.position = _position;
 
-        foreach (Collider _collider in l_colliders)
+
+        Debug.Log(l_colliders[0].name);
+
+
+
+        foreach (Collider2D _collider in l_colliders)
         {
             if(_collider.tag == "MainCharacter")
             {
